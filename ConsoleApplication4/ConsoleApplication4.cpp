b73cbdc4104f22d320bd78b0c9cfda8bb29f5e7e
matchastdy <matchastdyblog@gmail.com>
2022-07-09 19:03:34 +0900

    [Mod] test2を追加

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+﻿
 
 #include <conio.h>
 #include <stdio.h>
@@ -11,19 +11,23 @@
 
 #include <pdhmsg.h>
 
+/*
+*  Pdhの基本的な使い方
+*/
 int test() {
 	PDH_HQUERY              hQuery;
 	PDH_HCOUNTER            hCounter;
 	PDH_FMT_COUNTERVALUE    fmtValue;
 	PDH_STATUS				status;
-	
+
 	/*1 クエリを作成する*/
 	status = PdhOpenQuery(NULL, 0, &hQuery);
 	if (status != ERROR_SUCCESS) {
 		return 1;
 	}
 
-	/*2 クエリにカウンターを追加する
+	/*
+	* 2 クエリにカウンターを追加する
 	* この例ではパフォーマンスカウンターでの「\Processor(_Total)\% Processor Time」を追加している
 	*/
 	status = PdhAddCounter(hQuery, TEXT("\\Processor(_Total)\\% Processor Time"), 0, &hCounter);
@@ -32,7 +36,8 @@ int test() {
 		return 1;
 	}
 
-	/*3 パフォーマンスデータを収集する
+	/*
+	* 3 パフォーマンスデータを収集する
 	* 3-1 多くのカウンターではデータ値を計算するために2つのサンプルデータが必要とのこと
 	* そのため、最初に一度サンプルデータの収集を行う
 	*/
@@ -43,7 +48,7 @@ int test() {
 		return 1;
 	}
 
-	// キーが押されるまでループ
+	/* キーが押されるまでループ*/
 	while (!_kbhit()) {
 		/*3-2 コレクション間で少なくとも１秒以上待機する*/
 		Sleep(1000);
@@ -52,20 +57,26 @@ int test() {
 		/*3-3 再度サンプルデータを収集する*/
 		PdhCollectQueryData(hQuery);
 
-		/*4 サンプル取得後、表示可能な値を計算する
+		/*
+		* 4 サンプル取得後、表示可能な値を計算する
 		* この例ではDOUBLEがたに変換して表示
 		*/
 		PdhGetFormattedCounterValue(hCounter, PDH_FMT_DOUBLE, NULL, &fmtValue);
 		printf("doubleValue = %.15f\n", fmtValue.doubleValue);
 	}
 
-	/*5 クエリのエータ収集が完了したら、けりを閉じてシステムリソースを開放する
+	/*
+	* 5 クエリのエータ収集が完了したら、けりを閉じてシステムリソースを開放する
 	* クエリに関連付けられているすべてのカウンターハンドルを閉じる
 	*/
 	PdhCloseQuery(hQuery);
 	return 0;
 }
 
+/*
+* PdhExpandWildCardPathとPdhGetCounterInfoを使い、
+* 特定のカウンターパスをすべて取得、表示する
+*/
 int test2() {
 	PDH_HQUERY              hQuery;
 	HCOUNTER				hCounter[512];
@@ -77,15 +88,17 @@ int test2() {
 		return 1;
 	}
 
-	/* ワイルドカードを含むカウンターパス
-	*  今回は\Process(*)\% Processor Timeとする。
+	/*
+	* 1 ワイルドカードを含むカウンターパス
+	* ここでは\Process(*)\% Processor Timeとする。
 	*/
-	LPCWSTR					wildCardPath = TEXT("\\Process(*)\\% Processor Time"); // 
-	unsigned long			pathListLength = 0; // カウンターパスリストのサイズ数
-	/* PdhExpandWildCardPathは2回実行しなければならない。
-	*  1回目は取得するカウンターパスリストのサイズを取得する。
-	*  ちなみに、カウンターパスリストを格納する変数はない（NULL）ので、
-	*  正常に動作していれば、statusはPDH_MORE_DATAが返ってくるはず。
+	LPCWSTR					wildCardPath = TEXT("\\Process(*)\\% Processor Time");
+	unsigned long			pathListLength = 0;
+	/*
+	* 2-1 PdhExpandWildCardPathは2回実行しなければならない。
+	* 1回目は取得するカウンターパスリストのサイズを取得する。
+	* ちなみに、カウンターパスリストを格納する変数はない（NULL）ので、
+	* 正常に動作していれば、statusはPDH_MORE_DATAが返ってくるはず。
 	*/
 	status = PdhExpandWildCardPath(NULL, wildCardPath, NULL, &pathListLength, 0);
 	if (status != ERROR_SUCCESS && status != PDH_MORE_DATA) {
@@ -93,38 +106,41 @@ int test2() {
 		return 1;
 	}
 
-	/* カウンターパスリストのサイズがunsigned longの最大値となっていないかを確認
-	*  最大値の場合、次のメモリ確保の時にバッファオーバフローが発生するため、処理を終了する。
-	*  （おそらく、そんなことはないとは思うが）
+	/*
+	* カウンターパスリストのサイズがunsigned longの最大値となっていないかを確認する。
+	* 最大値の場合、次のメモリ確保の時にバッファオーバフローが発生するため、処理を終了する。
+	* （おそらく、そんなことはないとは思うが）
 	*/
 	if (pathListLength == ULONG_MAX) {
 		PdhCloseQuery(hQuery);
 		return 1;
 	}
 
-	/* カウンターパスリストのサイズ + 1の分だけ、メモリを確保する。
-	*  カウンターパスは最後はNULL文字で終わっていないといけないので、その分が増えている。
+	/*
+	* カウンターパスリストのサイズ + 1の分だけ、メモリを確保する。
+	* カウンターパスは最後はNULL文字で終わっていないといけないので、その分が増えている。
 	*/
-	TCHAR*					expandedPathList = (TCHAR*)calloc(pathListLength + 1, sizeof(TCHAR));
-	/* PdhExpandWildCardPathを再度実行し、*/
+	TCHAR* expandedPathList = (TCHAR*)calloc(pathListLength + 1, sizeof(TCHAR));
+	/* 2-2 PdhExpandWildCardPathを再度実行し、expandedPathListにデータを格納する*/
 	PdhExpandWildCardPath(NULL, wildCardPath, expandedPathList, &pathListLength, 0);
 
 
-	TCHAR*					epl = expandedPathList;
-	int						pathListNum = 0;
+	TCHAR* epl = expandedPathList;
+	int		pathListNum = 0;
 
 	if (epl == NULL) {
 		return 0;
 	}
 
 	while (*epl) {
+		/* 終端がNULL文字の取得したカウンターパスリストを一つ追加する*/
 		PdhAddCounter(hQuery, epl, 0, &hCounter[pathListNum]);
 		pathListNum++;
-		printf("%zd\n", wcslen(epl));
-		epl += wcslen(epl);
-		epl += 1;
-		//epl += wcslen(epl) + 1;
+		/* 次のカウンターパスが格納されているアドレスに移動する*/
+		epl += wcslen(epl) + 1;
 	}
+
+	free(expandedPathList);
 
 	status = PdhCollectQueryData(hQuery);
 	if (status != ERROR_SUCCESS) {
@@ -133,109 +149,41 @@ int test2() {
 	}
 
 	while (!_kbhit()) {
-		Sleep(10000);
+		Sleep(1000);
 		PdhCollectQueryData(hQuery);
+		/* カウンターパスの現在値を一つずつ取得する*/
 		for (int i = 0; i < pathListNum; i++) {
-			unsigned long		pdwBufferSize = 0;
+			unsigned long		bufferSize = 0;
+			/*
+			* 3 PdhGetCounterInfoを実行して、現在のカウンターの情報を取得する。
+			* PdhExpandWildCardPathと同様、2回実行しなければならない。
+			*/
+			status = PdhGetCounterInfo(hCounter[i], TRUE, &bufferSize, NULL);
+			if (status != ERROR_SUCCESS && status != PDH_MORE_DATA) {
+				PdhCloseQuery(hQuery);
+				return 1;
+			}
+			PPDH_COUNTER_INFO	pdhConterInfo = (PPDH_COUNTER_INFO)calloc(bufferSize, sizeof(PPDH_COUNTER_INFO));
+			PdhGetCounterInfo(hCounter[i], TRUE, &bufferSize, pdhConterInfo);
 
-			PdhGetCounterInfo(hCounter[i], TRUE, &pdwBufferSize, NULL);
-
-			PPDH_COUNTER_INFO lpBuffer = (PPDH_COUNTER_INFO)calloc(pdwBufferSize, sizeof(PPDH_COUNTER_INFO));
-			PdhGetCounterInfo(hCounter[i], TRUE, &pdwBufferSize, lpBuffer);
-
-			if (lpBuffer == nullptr) {
+			if (pdhConterInfo == nullptr) {
 				continue;
 			}
+			printf("%ls ", pdhConterInfo->szFullPath);
 
-			printf("%ls ", lpBuffer->szFullPath);
+			free(pdhConterInfo);
 
 			PdhGetFormattedCounterValue(hCounter[i], PDH_FMT_DOUBLE, NULL, &fmtValue[i]);
-
 			printf("doubleValue = %.15f\n", fmtValue[i].doubleValue);
-
-			free(lpBuffer);
-			lpBuffer = NULL;
 		}
 	}
 
-	/*5 クエリのエータ収集が完了したら、けりを閉じてシステムリソースを開放する
-	* クエリに関連付けられているすべてのカウンターハンドルを閉じる
-	*/
 	PdhCloseQuery(hQuery);
 	return 0;
 }
 
 int main()
 {
-	//test();
+	// test();
 	test2();
-
-	//PDH_HQUERY              hQuery;
-	//HCOUNTER				hCounter[512];
-	//PDH_FMT_COUNTERVALUE	fmtValue[512];
-
-	//TCHAR* cp;
-
-	//int     numProcessor = 0;
-
-	//HRESULT hr;
-
-	//hr = PdhOpenQuery(NULL, 0, &hQuery);
-
-	//if (hr != ERROR_SUCCESS) {
-	//	return 0;
-	//}
-
-
-	//LPCWSTR  wildCardPath = TEXT("\\Process(*)\\% Processor Time");
-	//unsigned long pathListLength = 0;
-	//PdhExpandWildCardPath(NULL, wildCardPath, NULL, &pathListLength, 0);
-
-	//TCHAR* mszExpandedPathList = (TCHAR*)calloc(pathListLength + 1, sizeof(TCHAR));
-
-	//PdhExpandWildCardPath(NULL, wildCardPath, mszExpandedPathList, &pathListLength, 0);
-
-	//cp = mszExpandedPathList;
-
-	//if (cp == NULL) {
-	//	return 0;
-	//}
-
-	//while (*cp) {
-	//	PdhAddCounter(hQuery, cp, 0, &hCounter[numProcessor]);
-	//	numProcessor++;
-	//	cp += wcslen(cp) + 1;
-	//}
-
-	//PdhCollectQueryData(hQuery);
-
-	//while (!_kbhit()) {
-	//	Sleep(10000);
-	//	//system("CLS");
-	//	PdhCollectQueryData(hQuery);
-	//	for (int i = 0; i < numProcessor; i++) {
-	//		unsigned long		pdwBufferSize = 0;
-
-	//		PdhGetCounterInfo(hCounter[i], TRUE, &pdwBufferSize, NULL);
-
-	//		PPDH_COUNTER_INFO lpBuffer = (PPDH_COUNTER_INFO)calloc(pdwBufferSize, sizeof(PPDH_COUNTER_INFO));
-	//		PdhGetCounterInfo(hCounter[i], TRUE, &pdwBufferSize, lpBuffer);
-
-	//		if (lpBuffer == nullptr) {
-	//			continue;
-	//		}
-
-	//		printf("%ls ", lpBuffer->szFullPath);
-
-	//		PdhGetFormattedCounterValue(hCounter[i], PDH_FMT_DOUBLE, NULL, &fmtValue[i]);
-
-	//		printf("doubleValue = %.15f\n", fmtValue[i].doubleValue);
-
-	//		free(lpBuffer);
-	//		lpBuffer = NULL;
-	//	}
-	//}
-	//_getch();
-	//PdhCloseQuery(hQuery);
-	//return 0;
 }
